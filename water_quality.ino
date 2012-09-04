fc2832b1e8bac6049d9a74d55d96241a55c71981
c piuggi <chris@piuggi.com>
2012-09-04 14:38:05 -0400

    EC print & get_ec added

--- water_quality.ino
+++ water_quality.ino
@@ -3,6 +3,7 @@
 void start_ph(){
   
  Serial2.begin(38400);
+ Serial2.setTimeout(5000);
  Serial2.print("L1\r");
  delay(1000);
  Serial2.print("L0\r");
@@ -23,6 +24,7 @@ float get_ph(){
 void start_ec(){
  
  Serial1.begin(38400);
+ Serial1.setTimeout(5000);
  Serial1.print("L1\r");
  delay(1000);
  Serial1.print("L0\r");
@@ -32,15 +34,27 @@ void start_ec(){
 }
 
 //-----------------------------------------------------------------------------------------
-void get_ec(){
+EC get_ec(){
+ EC t;
  Serial.println("R\\r --> EC"); //let us know we are asking
  Serial1.print("R\r"); //ask device
  delay(500);
  
- Serial.print( Serial1.parseInt() );
- Serial.println();
- Serial.print( Serial1.parseInt() );
- Serial.println();
- Serial.print( Serial1.parseFloat() );
+ t.conductivity= Serial1.parseInt() ;
+ t.tds = Serial1.parseInt();
+ t.salinity = Serial1.parseFloat();
  
+ return t;
+ 
+}
+
+void print_ec(EC &_t){
+
+  Serial.print("Conductivity \t");
+  Serial.println(_t.conductivity);
+  Serial.print("tds \t");
+  Serial.println(_t.tds);
+  Serial.print("salinity \t");
+  Serial.println(_t.salinity);
+  
 }
