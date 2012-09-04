1468a688a219055b3723d142666081d1d27bc74c
c piuggi <chris@piuggi.com>
2012-09-04 15:14:16 -0400

    Added temperature functions

--- Windowfarm_Hardware.ino
+++ Windowfarm_Hardware.ino
@@ -1,3 +1,6 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
 #include "data_types.h"
 
 void setup() {
@@ -5,6 +8,7 @@ void setup() {
  Serial.begin(9600); 
  start_ph();
  start_ec();
+ setup_temp();
  
  
 }
@@ -14,5 +18,7 @@ void loop() {
   Serial.println(get_ph());
   EC ec = get_ec();
   print_ec(ec);
+  temps celcius = return_temps();
+  print_temps(celcius);
   delay(4000);
 }
