b6f120230485ddfe59ec2c89588890e301265315
c piuggi <chris@piuggi.com>
2012-09-04 20:16:14 -0400

    Added WiFly

--- Windowfarm_Hardware.ino
+++ Windowfarm_Hardware.ino
@@ -2,7 +2,11 @@
 #include <DallasTemperature.h>
 #include <Wire.h>
 #include <TSL2561.h>
+#include <SPI.h>
+#include <WiFly.h>
+#include "keys.h"
 #include "data_types.h"
+
 
 #define BAUD 9600
 #define DATABAUD 38400
@@ -14,28 +18,35 @@ void setup() {
  start_ec();
  setup_temp();
  setup_co2();
+ setup_wifly();
  
 }
 
-
 void loop() {
   
-  TEMPS currentTemp = return_temps();
-  print_temps(currentTemp);
+  AllVals sensor;
   
-  Serial.println(get_ph(currentTemp.water));
+  sensor._temp = return_temps();
+  print_temps(sensor._temp);
   
-  EC ec = get_ec(currentTemp.water);
-  print_ec(ec);
+  sensor._ph = get_ph(sensor._temp.water);
+  Serial.println(sensor._ph);
+  
+  sensor._ec = get_ec(sensor._temp.water);
+  print_ec(sensor._ec);
+  
+  sensor._hum = return_rH(sensor._temp.air);
   
   Serial.print("Relative Humidity: \t");
-  Serial.println(return_rH(currentTemp.air));
+  Serial.println(sensor._hum);
   
+  sensor._co2 = return_co2();
   Serial.print("CO2 \t");
-  Serial.println(return_co2());
+  Serial.println(sensor._co2);
   
-  Light currentLight = get_light();
-  print_light(currentLight);
+  sensor._light = get_light();
+  print_light(sensor._light);
   
+  send_data(sensor);
   delay(4000);
 }
